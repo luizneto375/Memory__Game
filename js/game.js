9159664ae71ba6ccbc7a578798afbb474cb9cbe6
luizneto375 <114784434+luizneto375@users.noreply.github.com>
2023-05-07 10:33:16 -0300

    finalizado parte do tempo que jogar ficara ativo

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
 const grid = document.querySelector('.grid');
 const spanPlayer = document.querySelector('.player');
+const timer = document.querySelector('.timer');
 
 const characters = [
     'im01',
@@ -33,7 +34,8 @@ const checkEndGame = () => {
     const disabledCards = document.querySelectorAll('.disabled-card');
 
     if (disabledCards.length === 28) {
-        alert('Parabéns, você conseguiu!')
+        clearInterval(this.loop);
+        alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML}`);
     }
 }
 
@@ -115,11 +117,22 @@ const loadGame = () => {
     });
 }
 
+const startTimer = () => {
+
+    this.loop = setInterval(() => {
+        const currentTime = +timer.innerHTML;
+        timer.innerHTML = currentTime + 1;
+    }, 1000);
+
+}
 
 window.onload = () => {
 
     spanPlayer.innerHTML = localStorage.getItem('player');
+    //startTimer();
 
     loadGame();
 }
+
+
 
