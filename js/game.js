5cf5bacac826e233874e75b5b5732180b458d4d0
luizneto375 <114784434+luizneto375@users.noreply.github.com>
2023-05-07 09:48:11 -0300

    aidionando header com e js pra sempre que novo jogador comecar atualizar o  nome desse jogador

--- js/game.js
+++ js/game.js
@@ -1,4 +1,5 @@
 const grid = document.querySelector('.grid');
+const spanPlayer = document.querySelector('.player');
 
 const characters = [
     'im01',
@@ -114,4 +115,11 @@ const loadGame = () => {
     });
 }
 
-loadGame();
+
+window.onload = () => {
+
+    spanPlayer.innerHTML = localStorage.getItem('player');
+
+    loadGame();
+}
+
